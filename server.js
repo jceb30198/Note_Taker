4c7ed81da0d9f719f54220cdef89bcf887ad24a3
Jack Ceballos <jackrceballos@gmail.com>
2020-12-18 22:56:36 -0500

    Added gitignore and the express methods needed.

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+const PORT = 3000;
+
+// Block to have JSON properly encoded and to serve the JavaScript to the public file directory
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("./public"));
+
