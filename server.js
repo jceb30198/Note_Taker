3a0b7673b59c177fd436c11b4ddf7fe3fb19bc6a
Jack Ceballos <jackrceballos@gmail.com>
2020-12-21 23:28:48 -0500

    Was able to get the id number to stay and able to post but I cannot get the info.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// Dependencies
 const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { json } = require("express");
 
 const PORT = 3000;
 
@@ -12,27 +14,46 @@ app.use(express.json());
 
 // These route the to the html files in the public folder
 app.get("/notes", (request, response) => {
-    response.sendFile(path.join(__dirname,"/public/notes.html"));
+    response.sendFile(path.join(__dirname,"./public/notes.html"));
 })
 // Forgot to add _dirname the first time. It is needed to return the directory that is running the script
-app.get("/", (request, response) => {
-    response.sendFile(path.join(__dirname,"/public/index.html"));
+app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname,"./public/index.html"));
 })
 
+// Variables that will be used in multiple times in the methods
+const jsonParse = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+let idNum = Math.floor(Math.random() * 50000);
 
 // Blocks of code that route the api to add/edit/delete
 
 // Gets the notes that were previously saved on db.json
 app.get("/api/notes", (request, response) => {
-    response.sendFile(path.join(__dirname, "db/db.json"));
+    response.sendFile(path.join(__dirname, "/db/db.json"));
 })
 
 // Posts the changes made and will be stored in the db.json file
 app.post("/api/notes", (request, response) => {
-
+    const newNote = request.body;
+    let id = idNum;
+    console.log(id);
+    jsonParse.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(newNote), (err) => {
+        if (err) throw err;
+    })
+    response.json(jsonParse);
+    console.log("Saved");
 })
 
 // Handles the delete requests for the notes
 app.delete("/api/notes/:id", (request, response) => {
+    let currentId = request.params.id;
+    console.log(currentId);
+    
+    response.json(jsonParse);
+    console.log("Deleted");
+})
 
+app.listen(PORT, () => {
+    console.log("Listening on PORT: " + PORT);
 })
